f2f9063f973a71b9ac6b0b58dbfd80e445453469
maximedasilva <maximedasilva40@gmail.com>
2018-10-11 17:32:32 +0200

    finish website

--- js/app.js
+++ js/app.js
@@ -5,10 +5,8 @@ function update(){
         // subtract some from the height b/c of the padding
         var height = $element.height();
         $('.container').css('backgroundPosition','50% '+(70+Math.round((pos/400) * 100) + '%'));
-        
+
         $('.separator').css('backgroundPosition','50% '+(Math.round((pos/1500) * 100) + '%'));
 
     };
-
-
 $(window).bind('scroll', update);
